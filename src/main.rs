501468eb579612f962f6eb354f47e8bd55cba7c4
ryu2u <ryu2u@qq.com>
2025-05-18 22:56:45 +0800

    创建目录为书名

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Error> {
     info!("=========== initializing ========");
     let base_url = "http://www.qiqixs.info/";
 
-    let book_str = "218988";
+    let book_str = "201698";
     parse_book_directory(base_url, book_str).await;
 
     Ok(())
@@ -26,20 +26,22 @@ async fn parse_book_directory(base_url: &str, book_str: &str) {
     let res = reqwest::get(&base_url).await.unwrap();
     let res = res.text().await.unwrap();
     let doc = Html::parse_document(&res);
+    let book_title = get_title(&doc);
+    println!("{}", book_title);
     let selector = Selector::parse("dl").unwrap();
-    let res = doc.select(&selector);
-    let html_vec: Vec<_> = res.map(|v| v).collect();
+    let html_vec: Vec<_> = doc.select(&selector).map(|v| v).collect();
     let dl = html_vec.first().unwrap();
     let a_selector = Selector::parse("a").unwrap();
     let a_vec: Vec<_> = dl.select(&a_selector).collect();
     let mut handle = vec![];
-    for (index,a_element) in a_vec.iter().enumerate() {
+    for (index, a_element) in a_vec.iter().enumerate() {
         let href = a_element.value().attr("href").unwrap().to_string();
         info!("{} - {}", a_element.inner_html(), href);
         let html = a_element.inner_html().clone();
         let base_url = base_url.clone();
+        let book_title = book_title.clone();
         handle.push(tokio::spawn(async move {
-            parse_book_content(&base_url, &href, html,index).await;
+            parse_book_content(&base_url, &href, html, index, book_title).await;
         }));
     }
     for t in handle {
@@ -47,7 +49,13 @@ async fn parse_book_directory(base_url: &str, book_str: &str) {
     }
 }
 
-async fn parse_book_content(base_url: &str, tail: &str, title: String,index: usize) -> Option<()>{
+async fn parse_book_content(
+    base_url: &str,
+    tail: &str,
+    article_title: String,
+    index: usize,
+    book_title: String,
+) -> Option<()> {
     let base_url = format!("{}/{}", base_url, tail);
     let mut res = String::new();
     let mut count = 0;
@@ -69,7 +77,7 @@ async fn parse_book_content(base_url: &str, tail: &str, title: String,index: usi
         None => {
             error!(
                 "download title : [{}] failed --> url : [{}] tail : [{}]",
-                title, base_url, tail
+                article_title, base_url, tail
             );
             return None;
         }
@@ -83,15 +91,29 @@ async fn parse_book_content(base_url: &str, tail: &str, title: String,index: usi
         }
     });
     str.remove(0);
-    str.insert(0, format!("{}\n\n", title));
-    if !Path::exists(Path::new("dir")) {
-        std::fs::create_dir_all("dir").unwrap();
+    str.insert(0, format!("{}\n\n", article_title));
+    if !Path::exists(Path::new(book_title.as_str())) {
+        std::fs::create_dir_all(&book_title).unwrap();
     }
-    std::fs::File::create(format!("dir/{}_{}.txt",index, title.replace(" ", "_")))
-        .unwrap()
-        .write_all(str.join("  ").as_bytes())
-        .unwrap();
+    std::fs::File::create(format!(
+        "{}/{}_{}.txt",
+        book_title,
+        index,
+        article_title.replace(" ", "_")
+    ))
+    .unwrap()
+    .write_all(str.join("  ").as_bytes())
+    .unwrap();
     Some(())
+}
+
+fn get_title(doc: &Html) -> String {
+    let selector_title = Selector::parse(".title").unwrap();
+    let selector_h1 = Selector::parse("h1").unwrap();
+    let title_doc: Vec<_> = doc.select(&selector_title).map(|v| v).collect();
+    let title_element = title_doc.first().unwrap();
+    let res: Vec<_> = title_element.select(&selector_h1).map(|v| v).collect();
+    res.first().unwrap().inner_html().to_string()
 }
 
 #[cfg(test)]
@@ -103,7 +125,16 @@ mod tests {
         let base_url = "http://www.qiqixs.info/195803/";
         let tail = "";
         let title = "".to_string();
-        let res = parse_book_content(base_url, tail, title,1).await;
+        let book_title = "195803".to_string();
+        let res = parse_book_content(base_url, tail, title, 1, book_title).await;
         res.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_book_title() {
+        let base_url = "http://www.qiqixs.info/";
+
+        let book_str = "201698";
+        parse_book_directory(base_url, book_str).await;
     }
 }
